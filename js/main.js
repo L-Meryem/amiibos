092cefd3d53fd2aea6331f47a22ac469d3fa1e9f
Meryem L. <82731366+L-Meryem@users.noreply.github.com>
2025-10-03 14:05:33 -0400

    Add amiibos filtered by date or year

---
+++ js/main.js
@@ -0,0 +1,83 @@
+/*
+figure not card
+filter by release date and location: na
+show
+    image
+    name
+    amiiboSeries
+    release date
+*/
+
+const button = document.querySelector('button');
+button.addEventListener('click', getAmiibos);
+
+
+function getAmiibos() {
+    clearCards();
+    const date = document.querySelector('#date').value;
+
+    const url = `https://www.amiiboapi.com/api/amiibo?type=figure&release.na=${date} `;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            //If an amiibo was released on the date you entered I will return it
+            //if not, I will return all amiibos tha come out that year
+            let filteredAmiibos;
+            const amiinoReleasedThatDay = data.amiibo.filter(fig => fig.release.na === date);
+
+            if (amiinoReleasedThatDay.length)
+                filteredAmiibos = amiinoReleasedThatDay;
+            else {
+                filteredAmiibos = data.amiibo.filter(fig => {
+                    const year = new Date(date).getFullYear();
+                    const releaseYear = new Date(fig.release.na).getFullYear();
+                    return year === releaseYear;
+                });
+            }
+            // console.log(filteredAmiibos);
+            filteredAmiibos.forEach(fig => {
+                console.log(fig);
+                // Create a card inside section cards
+                const { h2, series, releaseDate, img } = createCard();
+                // Fill in the card
+                h2.innerText = fig.name;
+                series.innerText = fig.amiiboSeries;
+                releaseDate.innerText = fig.release.na;
+                img.src = fig.image;
+                img.alt = fig.name;
+            })
+        })
+        .catch(error => console.log(error));
+
+}
+
+function clearCards(){
+    const cards = document.querySelector('.cards')
+    cards.replaceChildren();
+}
+
+function createCard() {
+    //Create a card
+    const cards = document.querySelector('.cards');
+    const div = document.createElement('div');
+    div.className = 'card';
+    //Create card's content
+    const h2 = document.createElement('h2');
+    h2.className = 'name';
+    const series = document.createElement('small');
+    series.className = 'series';
+    const releaseDate = document.createElement('small');
+    releaseDate.className = 'release_date';
+    const img = document.createElement('img');
+    img.src = '';
+    img.alt = '';
+    //Adding style
+    div.classList.add('card');
+    img.classList.add('image');
+    //Connect tags
+    div.append(h2, series, releaseDate, img);
+    cards.append(div);
+    //return tags as an {} to diconstruct
+    return { h2, series, releaseDate, img };
+}
